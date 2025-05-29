fae52f3253fe4e0640939e6e60bec66f467efadd
jeey <wldudsmile97@gmail.com>
2025-05-29 17:29:59 +0900

    todos add&조회 완료

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FASTAPI_TODOapp
 FASTAPI 활용한 todo 앱 만들기
+
+# 설치라이브러리 
+
+# 실행방법 
+```
+
+```
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from database import engine, SessionLocal 
+from sqlalchemy.orm import Session
+import models
 import os
+from fastapi import status
+
 
 # FastAPI() 객체 생성
 app = FastAPI()
+
+# models에 정의한 모든 클래스, 연결한 DB엔진에 테이블로 생성
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        # 마지막에 무조건 닫음
+        db.close()
 
 abs_path = os.path.dirname(os.path.realpath(__file__))
 # print(abs_path)
@@ -19,9 +35,14 @@ app.mount("/static", StaticFiles(directory=f"{abs_path}/static"), name="static")
 
 
 @app.get("/")
-async def home(request: Request):
-    todo = 20
-    #html 파일에 데이터 랜더링 해서 리턴한다는 의미 
-    return templates.TemplateResponse("index.html",
-                                      {"request": request,
-                                       "todos": todo})
+async def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).order_by(models.Todo.id.desc())
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+
+@app.post("/add")
+async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
+    todo = models.Todo(task=task)
+    db.add(todo)
+    db.commit()
+    # 조회 수행하는 path(함수)로 제어권을 넘김 
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
